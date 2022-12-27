de3265fd3b803e0e13b5b4755fcb8b6a6f56ea00
Giwon <leecode99@gmail.com>
2022-12-26 22:20:24 -0500

    Adding only source files

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Header from "./component/Header";
 import EmptyPage from "./component/EmptyPage";
 import CreateWord from './component/CreateWord';
 import CreateDay from './component/CreateDay';
-
+import DelDay from './component/DelDay';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+// npm start
+// json-server --watch ./src/db/data.json --port 8000
 
 export default function App() {
   return (
@@ -20,7 +22,7 @@ export default function App() {
           <Route path="/day/:day" element={<Day/>} />
           <Route path="/create_word" element={<CreateWord/>} />
           <Route path="/create_day" element={<CreateDay/>} />
-          {/* <Route path="/Del_day" element={<DelDay/>} /> */}
+          <Route path="/Del_day" element={<DelDay/>} />
           <Route path="*" element={<EmptyPage/>}/>
         </Routes>
       </div>
